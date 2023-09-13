7d952eea4e08139bd57bc300502ec07930aa3da1
IMXeno123 <zzxxeno@outlook.com>
2023-09-13 20:25:22 +0800

    Add files via upload
    Perfect the code

---
+++ bin/text_global_replace.py
@@ -0,0 +1,38 @@
+﻿from math import log
+import re
+import os
+
+def sub_by_dir(directory:str = False, old_text:str = False, new_text:str = False, isMemory:int = 1):
+    """
+    Need import re and os modules！
+    """
+    try:
+        while True:
+            mode = input("""**選擇模式**
+-輸入 1 多行文字查找替換
+-輸入 2 正則表達式替換
+-輸入任意其他數字退出
+""")
+            # mode 1
+            if mode == "1":
+                while True:
+                    if old_text:
+                        if isMemory == 1:
+                            memory_old = input("按enter繼續使用上次表達式, 任意字符串修改表達式：\n")
+                            if memory_old == "":
+                                old_text = False
+                                continue
+                        elif isMemory == 0:
+                            pass
+                        if memory_old == "":
+                            break
+                    else:
+                        old_text = input("請輸入需要查找的多行文字, 換行符請用‘\\n’表示： \n").replace("\\n", "\n")
+                        break
+            
+    except Exception as error:
+        print(print(f"遇到錯誤：{error}"))
+
+
+
+sub_by_dir()
