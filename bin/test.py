3727be38125b588ce529b2b143e3b748f41422a5
xs <xs@asheng.onmicrosoft.com>
2024-01-22 06:57:45 -0800

    update

--- bin/test.py
+++ bin/test.py
@@ -1,13 +1,10 @@
-﻿import turtle as t
-Pen1 = t.Pen()
-Pen1.pencolor("red")
-Pen1.pensize(3)
-for i in range(3):
-    for j in range(2):
-        Pen1.forward(100)
-        Pen1.left(90)
-        Pen1.forward(200)
-        Pen1.left(90)
-    Pen1.left(120)
-Pen1.hideturtle()
-t.done()
+﻿if 0:
+    pass
+elif 0:
+    pass
+elif 0:
+    pass
+elif 0:
+    pass
+else:
+    pass
