014cf2b6fe3f8f75bf36d900a40e0c722a441f40
xs <xs@asheng.onmicrosoft.com>
2024-01-25 06:54:54 -0800

    update

--- bin/test.py
+++ bin/test.py
@@ -15,12 +15,35 @@
 # else:
 #     print("请输入正确的两位正整数")
 
+# while True:
+#     num = input('请输入一个两位正整数')
+#     if int(num) >= 10 and int(num) < 100 and len(num) == 2:
+#         b = 0
+#         for i in range(len(num)):
+#             b += int(num[i])
+#         print(b)
+#     else:
+#         print("请输入正确的两位正整数")
+
 while True:
-    num = input('请输入一个两位正整数')
-    if int(num) >= 10 and int(num) < 100 and len(num) == 2:
-        b = 0
-        for i in range(len(num)):
-            b += int(num[i])
-        print(b)
+    num1 = int(input('请输入第一个整数'))
+    num2 = int(input('请输入第一个整数'))
+    if num1 < num2:
+        sum = 0
+        for i in range(num1,num2+1):
+            sum += i
+            print(sum)
+        
     else:
-        print("请输入正确的两位正整数")
+        print('NO')
+
+# a=int(input())
+# b=int(input())
+# if a<b:
+#     sum=0
+#     print(list(range(a,b+1)))
+#     for i in range(a,b+1):
+#         sum+=i
+#     print(sum)
+# else:
+#     print("NO")
